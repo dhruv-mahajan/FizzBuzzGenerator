6846a383f1b616814fd95518d8b4b42c846bd45c
Dhruv Mahajan <dhruv.mahajan@tcs.com>
2019-01-30 15:34:34 +0530

    Main method removed

--- src/main/java/dummyPack/FizzBuzz.java
+++ src/main/java/dummyPack/FizzBuzz.java
@@ -1,36 +1,18 @@
 package dummyPack;
 
-import java.util.Scanner;
-
 public class FizzBuzz {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the number to test against : ");
-        int number = 0;
-        String result = null;
-        if (scanner.hasNext()) {
-            number = Integer.parseInt(scanner.next());
-        }
-        if (number > 0) {
-            result = getFizzBuzzOutput(number);
-        } else {
-            System.out.println(number);
-        }
-
-        System.out.println(result);
-    }
 
     public static String getFizzBuzzOutput(int number) {
 
-        if (number % 15 == 0) {
-            return "FizzBuzz";
-        } else if (number % 5 == 0) {
-            return "Buzz";
-        } else if (number % 3 == 0) {
-            return "Fizz";
-        } else {
-            return String.valueOf(number);
+        if (number > 0) {
+            if (number % 15 == 0) {
+                return "FizzBuzz";
+            } else if (number % 5 == 0) {
+                return "Buzz";
+            } else if (number % 3 == 0) {
+                return "Fizz";
+            }
         }
-
+        return String.valueOf(number);
     }
 }
